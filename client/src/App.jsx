42eeb6f65cec4913cd63a137ca127e515437bb29
Sagar <sagarwaghmare1384@gmail.com>
2025-08-29 00:39:35 +0530

    Login page done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import UserLayout from './components/Layout/UserLayout'
 import AdminLayout from './components/Layout/AdminLayout'
 import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
 import { Toaster } from 'sonner'
 const App = () => {
   return (
@@ -11,6 +13,8 @@ const App = () => {
       <Routes>
         <Route path="/" element={<UserLayout />} >
         <Route index element={<Home/>}/>
+        <Route path='Login' element={<Login/>}/>
+        <Route path='register' element={<Register/>}/>
         </Route>
         <Route path="/admin" element={<AdminLayout />} />
       </Routes>
